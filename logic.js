5711acbbdd7465a896de119a9d7f8d8dcd25983b
hegratrix <gratrixmomma@yahoo.com>
2018-09-11 12:30:45 -0700

    stylized

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 let giphyArr = ["Hello", "Good-Bye", "Hey", "Good Morning", "Good Night", "Sweet Dreams", "Good Luck", "Thinking of You", "Sorry", "Have a Great Day", "Get Well", "Love You", "Miss You"]
+let btnColor = ["#ff6600", "#00cccf", "#fed700"]
 let chosenGreeting = ""
 let apiKey= 'Cq9dqB1rAP15arh4r7m2imChIUIf7mcl'
-let firstClick = true
 
 // makes buttons when page is loaded
 $(document).ready(function (){
@@ -14,7 +14,7 @@ $('#add-greeting').click(function (event) {
     let newGreeting = $('#greeting-input').val()
     giphyArr.push(newGreeting)
     makeButtons()
-    $('#greeting-input').empty()
+    $('#greeting-input').val('')
 })
 
 // makes buttons
@@ -22,17 +22,20 @@ function makeButtons (){
     $('#greeting-buttons').empty()
     for (let i = 0; i < giphyArr.length; i++) {
         let title = giphyArr[i]
+        let color = btnColor[Math.floor(Math.random() * btnColor.length)]
+        console.log(color)
         $('#greeting-buttons').append (`
-            <button id ="btn${title}" class ="greeting-button" onclick="getPictures('${title}')">${title}</button>
+            <button id ="btn${title}" class ="greeting-button" style ="background-color: ${color}" onclick="getPictures('${title}')">${title}</button>
         `)
     }
 }
 
 // generates 10 rated g/pg gifs
-function getPictures (chosenGreeting){
+function getPictures (greeting){
     $('#greeting-gifs').empty()
     let picNumber = 10
-    $.get('http://api.giphy.com/v1/gifs/search?q='+chosenGreeting+'&api_key='+apiKey+'&limit=15')
+    let frame = 1
+    $.get('http://api.giphy.com/v1/gifs/search?q='+greeting+'&api_key='+apiKey+'&limit=15')
     .then(function (r){
         for (let i = 0; i <picNumber; i++) {
             let rating = (r.data[i].rating)
@@ -40,13 +43,14 @@ function getPictures (chosenGreeting){
                 let clip = (r.data[i].images.original.url)
                 let still = (r.data[i].images.original_still.url)
                 $('#greeting-gifs').append (`
-                    <div class="gif-div">
+                    <div class="gif-div" style="border-image: url(./images/frame${frame}.png) 240 round;">
                         <img id ="pic${i}" class="clip" src="${clip}" onclick ="switchPic('still', ${i})">
                         <img id ="still${i}" class="still" src ="${still}" onclick ="switchPic('clip', ${i})">
-                        <h3>'Rating: '${rating}</h3>
+                        <h3 id ="rating">'Rating: '${rating}</h3>
                     </div>
                 `)
                 $('.still').css("display", "none")
+                frame++
             } else {
              picNumber++
             }
@@ -59,8 +63,6 @@ function getPictures (chosenGreeting){
 
 // click on picture change from gif to still to back
 function switchPic (type, id) {
-    console.log(type)
-    console.log(`'#pic${id}'`)
         if (type==='still'){
         $(`#pic${id}`).css("display", "none")
         $(`#still${id}`).css("display", "initial")
